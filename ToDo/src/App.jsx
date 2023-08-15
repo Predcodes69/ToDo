37fd9dc25f23a1fb010a41a77bf5e1de26c25a7a
Zaid Alam <predcodes695@gmail.com>
2023-08-15 22:35:30 +0530

    add:Added eslint, firebase, zustand, tailwind

---
+++ ToDo/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import './index.css'
+const App = () => {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ ToDo/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVfKsvaR5eCwLLZ05CeMLD3psuHs33j-0",
+  authDomain: "todo-e7ed2.firebaseapp.com",
+  projectId: "todo-e7ed2",
+  storageBucket: "todo-e7ed2.appspot.com",
+  messagingSenderId: "613578546520",
+  appId: "1:613578546520:web:7f4e57d57d5b6cf473592d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
